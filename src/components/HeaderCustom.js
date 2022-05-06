5f4f06f1e30be530370df610bb84a309249b7fc2
Margus <agusnicolas21@gmail.com>
2022-05-06 20:15:26 +0100

    Overlay menu

--- src/components/HeaderCustom.js
+++ src/components/HeaderCustom.js
@@ -1,29 +1,56 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function HeaderCustom() {
 
   const navigate = useNavigate();
+  const [visible, setVisible] = useState("item-visible");
+
+  const handleVisible = () => {
+    setVisible(visible ? "item-unvisible" : "item-visible");
+  }
+  const items = [
+    {
+      name: "Rutas",
+      dir: "/all-routes"
+    },
+    {
+      name: "Conócenos",
+      dir: "/about-us"
+    }
+  ]
+
+  const showItems = (item) => {
+    return (
+      <h3 onClick={() => navigate(item.dir)}>{item.name}</h3>
+    )
+  }
 
   return (
     <>
       <div className="custom-header">
+        <div className="overlay-menu">
+          <img className={`logo ${visible ? "item-unvisible" : "item-visible"}`} width="200" height="300" src="/img/LogoSabiosGuias.png" alt="Sabios Guías Intérpretes" />
+        </div>
         <div className="custom-header-elements">
-          <img
+          <img className={`logo ${visible}`}
             width="200"
             height="300"
             src="/img/LogoSabiosGuias.png"
             alt="Sabios Guías Intérpretes"
           />
           <img
-            className="menu"
+            onClick={handleVisible}
+            className={`menu ${visible}`}
             width="200"
             height="300"
             src="/img/menu_mov.png"
             alt="Sabios Guías Intérpretes"
           />
           <div className="items">
-            <h3 onClick={navigate(`/`)}>Rutas</h3>
-            <h3>Conócenos</h3>
+            {
+              items.map(showItems)
+            }
           </div>
         </div>
       </div>
