2e62cf1b629928bc8b76898527068eaeb417fa61
Margus <agusnicolas21@gmail.com>
2022-05-09 19:37:49 +0100

    Main Version 1.0.1

---
+++ src/components/infoSabios.js
@@ -0,0 +1,17 @@
+export const infoSabios = [
+    {
+        id: 1,
+        img: "/img/educacion_icon.png",
+        text: "El proyecto Sabios Guías Intérpretes pretende acercarse aun más a la población juvenil a través del Programa enSeñas con el objetivo de incorporar de manera integral nuestros valores históricos, culturales y patrimoniales a la práctica educativa, entendida en su generalidad: formal, no formal e informal."
+    },
+    {
+        id: 2,
+        img: "/img/guias_icon.png",
+        text: "Te proponemos una nueva forma de recorrer nuestros pueblos y municipios. De la mano de nuestros mayores podrás vivir de primera mano los usos originales de los lugares de interés y sus vivencias de antaño. Disfruta de la Naturaleza y también de la Historia."
+    },
+    {
+        id:3,
+        img: "/img/vidassabias_icon.png",
+        text:"Vidas Sabias nace con el objetivo de fomentar la recopilación audiovisual del patrimonio cultural inmaterial que las personas de Canarias nos aportan. Un reconocimiento al valor del saber popular y que no está escrito. Necesario para conocer y entender la riqueza cultural que nos rodea."
+    }
+];
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,7 +1,9 @@
 import { useNavigate } from "react-router-dom";
+import { infoSabios } from "../components/infoSabios";
 import DesktopBg from "../components/DesktopBg";
 import FooterCustom from "../components/FooterCustom";
 import HeaderCustom from "../components/HeaderCustom";
+import InfoSlider from "../components/InfoSlider";
 import MobileBg from "../components/MobileBg";
 import "../css/app.css";
 
@@ -15,6 +17,13 @@ export default function MainPage() {
             <MobileBg />
             <DesktopBg />
             <HeaderCustom />
+            <div className="section">
+                <div className="section-elements">
+                    {
+                        infoSabios.map((item) => <InfoSlider image={item.img} info={item.text}/>)
+                    }
+                </div>
+            </div>
             <FooterCustom />
         </>
     )
